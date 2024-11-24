43a691defddb24c5b53577dd9568f7b4b693bcdd
AlexMan123456 <alex.man03888@gmail.com>
2024-11-24 14:04:26 +0000

    Refactored execution functions for each command

--- commands/utility/ping.js
+++ commands/utility/ping.js
@@ -1,11 +1,13 @@
 const { SlashCommandBuilder } = require('discord.js');
 
+async function execute(interaction){
+	await interaction.reply("pong")
+}
+
 module.exports = {
 	cooldown: 5,
 	data: new SlashCommandBuilder()
 		.setName('ping')
 		.setDescription('Replies with pong'),
-	async execute(interaction) {
-		await interaction.reply("pong")
-	},
+	execute,
 };
