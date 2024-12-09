05d4da18758e7efade5c282e4cb82e8f099d74ad
AlexMan123456 <alex.man03888@gmail.com>
2024-12-09 00:36:03 +0000

    Added suggest feature

--- commands/utility/suggest.js
+++ commands/utility/suggest.js
@@ -1,8 +1,18 @@
-const { SlashCommandBuilder } = require("discord.js")
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js")
 
 async function execute(interaction){
+    const suggestionTitle = interaction.options.getString("title")
+    const suggestionDescription = interaction.options.getString("description")
+
+    const embed = new EmbedBuilder()
+        .setTitle(suggestionTitle)
+        .setAuthor({name: interaction.user.globalName})
+        .addFields({name: "Details", value: suggestionDescription})
+        .setTimestamp()
+
     const suggestionsChannel = await findSuggestionsChannel(interaction)
-    await interaction.guild.channels.cache.get(suggestionsChannel[0]).send({content: suggestionsChannel[1].name})
+    await interaction.guild.channels.cache.get(suggestionsChannel[0]).send({embeds: [embed]})
+    await interaction.reply("Suggestion noted")
 }
 
 function findSuggestionsChannel(interaction){
@@ -19,6 +29,18 @@ function findSuggestionsChannel(interaction){
 module.exports = {
     data: new SlashCommandBuilder()
         .setName("suggest")
-        .setDescription("Suggest a feature to be added to the bot"),
+        .setDescription("Suggest a feature to be added to the bot")
+        .addStringOption((option) => {
+            return option
+                .setName("title")
+                .setDescription("The title of your suggestion")
+                .setRequired(true)
+        })
+        .addStringOption((option) => {
+            return option
+                .setName("description")
+                .setDescription("Describe the main features of your suggestion")
+                .setRequired(true)
+        }),
     execute
 }
