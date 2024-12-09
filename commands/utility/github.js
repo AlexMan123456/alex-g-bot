bbf0f0d90e150a987bb39816f7176d631aa31722
AlexMan123456 <alex.man03888@gmail.com>
2024-12-09 15:14:46 +0000

    Added error messages to commands

--- commands/utility/github.js
+++ commands/utility/github.js
@@ -1,11 +1,15 @@
-const { SlashCommandBuilder } = require("discord.js");
+const { SlashCommandBuilder, MessageFlags } = require("discord.js");
 
 const data = new SlashCommandBuilder()
 .setName("github")
 .setDescription("Link to GitHub repository for this bot")
 
 async function execute(interaction){
-    await interaction.reply("Here's the GitHub repository for my code: https://github.com/AlexMan123456/alex-g-bot")
+    try{
+        await interaction.reply("Here's the GitHub repository for my code: https://github.com/AlexMan123456/alex-g-bot")
+    }catch(err){
+        await interaction.reply({content: `${err}`, flags: MessageFlags.Ephemeral})
+    }
 }
 
 module.exports = { data, execute }
--- commands/utility/ping.js
+++ commands/utility/ping.js
@@ -1,11 +1,15 @@
-const { SlashCommandBuilder } = require('discord.js')
+const { SlashCommandBuilder, MessageFlags } = require('discord.js')
 
 const data = new SlashCommandBuilder()
 .setName('ping')
 .setDescription('Replies with pong')
 
 async function execute(interaction){
-	await interaction.reply("pong")
+	try{
+		await interaction.reply("pong")
+	} catch(err){
+		await interaction.reply({content: `${err}`, flags: MessageFlags.Ephemeral})
+	}
 }
 
 module.exports = { cooldown: 5, data, execute }
