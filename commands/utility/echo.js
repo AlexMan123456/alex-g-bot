4239b03a52dc2e7295f8073d820dadaa7bd7f998
AlexMan123456 <alex.man03888@gmail.com>
2024-12-09 14:30:36 +0000

    Refactored so that the data property is a variable passed into the object being exported

--- commands/utility/echo.js
+++ commands/utility/echo.js
@@ -1,17 +1,16 @@
 const { SlashCommandBuilder } = require("discord.js")
 
+const data = new SlashCommandBuilder()
+.setName("echo")
+.setDescription("Returns the given input")
+.addStringOption((option) => {
+    return option
+        .setName("input")
+        .setDescription("The input to echo back")
+})
+
 async function execute(interaction){
     await interaction.reply(interaction.options.getString("input"))
 }
 
-module.exports = {
-    data: new SlashCommandBuilder()
-        .setName("echo")
-        .setDescription("Returns the given input")
-        .addStringOption((option) => {
-            return option
-                .setName("input")
-                .setDescription("The input to echo back")
-        }),
-    execute
-}
+module.exports = { data, execute }
