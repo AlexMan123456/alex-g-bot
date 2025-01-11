09f7d7a24d916f9ef7e7d6c9f51c91844cde591c
AlexMan123456 <alex.man03888@gmail.com>
2025-01-11 02:45:56 +0000

    Added ability to set error log channel

--- src/utils/log-error.js
+++ src/utils/log-error.js
@@ -1,20 +1,14 @@
 const { EmbedBuilder } = require("discord.js")
 const findChannel = require("./find-channel")
+const { getGuildById } = require("../database-interactions/guilds")
 
 function logError(client, error){
-    return findChannel(client, "error-log").then((errorChannelArray) => {
-        return client.guild.channels.cache.get(errorChannelArray[0])
+    return getGuildById(client.guild.id).then((guild) => {
+        return client.guild.channels.cache.get(guild.error_log_id)
     }).then((errorChannel) => {
         const errorMessage = new EmbedBuilder()
         .setTitle("An error has occured")
         .setDescription(`${error}`)
-        .setColor("Red")
-
-        errorChannel.send({embeds: [errorMessage]})
-    }).catch((err) => {
-        const errorMessage = new EmbedBuilder()
-        .setTitle("An error has occured")
-        .setDescription(`${err}`)
         .setColor("Red")
 
         errorChannel.send({embeds: [errorMessage]})
