2f5b07649a5922dd29ad65d39e4cf6e561593f55
AlexMan123456 <alex.man03888@gmail.com>
2025-01-18 17:49:54 +0000

    Added correct answer console.log in development mode

--- src/commands/economy/quiz.js
+++ src/commands/economy/quiz.js
@@ -38,6 +38,9 @@ class QuizCommand extends Command {
             const {data} = await axios.get(apiLink)
             const quizQuestion = data.results[0]
             const allAnswers = this.setupAnswers(quizQuestion.incorrect_answers, quizQuestion.correct_answer)
+            if(process.env.NODE_ENV === "development"){
+                console.log(quizQuestion.correct_answer)
+            }
 
             const optionChoices = ["A", "B", "C", "D"]
     
