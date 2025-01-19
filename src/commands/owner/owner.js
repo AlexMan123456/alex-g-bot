f38dbf2ceac4788494264a7dcdd1126fc36448a7
AlexMan123456 <alex.man03888@gmail.com>
2025-01-19 16:59:05 +0000

    Deleted unnecessary owner subcommands since they're now database subcommands

--- src/commands/owner/owner.js
+++ src/commands/owner/owner.js
@@ -20,24 +20,8 @@ class OwnerCommand extends Subcommand {
                     default: true
                 },
                 {
-                    name: "query-database",
-                    chatInputRun: "chatInputQueryDatabase"
-                },
-                {
                     name: "eval",
                     chatInputRun: "chatInputEval"
-                },
-                {
-                    name: "add-user-to-db",
-                    chatInputRun: "chatInputAddUserToDatabase"
-                },
-                {
-                    name: "add-guild-to-db",
-                    chatInputRun: "chatInputAddGuildToDatabase"
-                },
-                {
-                    name: "add-user-to-guild",
-                    chatInputRun: "chatInputAddUserToGuild"
                 },
                 {
                     name: "set-mod-role",
@@ -59,45 +43,8 @@ class OwnerCommand extends Subcommand {
                 })
                 .addSubcommand((command) => {
                     return command
-                        .setName("query-database")
-                        .setDescription("Query the database using a raw SQL string")
-                        .addStringOption((option) => {
-                            return option
-                                .setName("query")
-                                .setDescription("The query string")
-                                .setRequired(true)
-                        })
-                })
-                .addSubcommand((command) => {
-                    return command
                         .setName("eval")
                         .setDescription("Evaluates some JavaScript code")
-                })
-                .addSubcommand((command) => {
-                    return command
-                        .setName("add-user-to-db")
-                        .setDescription("Add a user to the bot's database")
-                        .addUserOption((option) => {
-                            return option
-                                .setName("user")
-                                .setDescription("The user to add to the database")
-                                .setRequired(true)
-                        })
-                })
-                .addSubcommand((command) => {
-                    return command
-                        .setName("add-guild-to-db")
-                        .setDescription("Add a guild to the bot's database")
-                })
-                .addSubcommand((command) => {
-                    return command
-                        .setName("add-user-to-guild")
-                        .setDescription("Add a user to a guild in the bot's database")
-                        .addUserOption((option) => {
-                            return option
-                                .setName("user")
-                                .setDescription("The user to add to the guild the command is being run in")
-                        })
                 })
                 .addSubcommand((command) => {
                     return command
